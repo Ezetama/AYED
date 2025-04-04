1743acfbddc210d50ce0778509081c301b94390d
Ezetama <tamame127@gmail.com>
2025-04-04 03:09:06 -0300

    .

---
+++ AYED/src/Practica1/Ejercicio4/ProgramaPrincipal.java
@@ -0,0 +1,14 @@
+package Practica1.Ejercicio4;
+
+public class ProgramaPrincipal {
+	public static void main(String[] args) {
+		int a = 1, b = 2;
+		Integer c = 3, d = 4;
+		Ejercicio4Clase.swap1(a, b);
+		Ejercicio4Clase.swap2(c,d);
+		System.out.println("a=" + a + " b=" + b) ;
+		System.out.println("c=" + c + " d=" + d) ;
+		 }
+		
+
+}
